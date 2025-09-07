e915667a064a96f1754de64c0698f8736d88dfd6
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-09-07 19:26:13 +0200

    Create 00_Table_Creation.sql

---
+++ 00_TableSetUp/00_Table_Creation.sql
@@ -0,0 +1,13 @@
+CREATE TABLE kickstarter (
+ID int,
+Name varchar(200),
+Category varchar(50),
+Subcategory varchar(50),
+Country varchar(50),
+Launched timestamp,
+Deadline timestamp,
+Goal int, 
+Pledged int,
+Backers int,
+State varchar(50)
+)
